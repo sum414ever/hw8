01122a91253b97e6cb5a11dae847ff73d4d2a00b
sum414ever <copokaalekc@gmail.com>
2019-07-15 12:33:27 +0300

    Dev: fixer all suport remarks

--- src/com/company/errors/memory/OutOfMemoryError.java
+++ src/com/company/errors/memory/OutOfMemoryError.java
@@ -1,6 +1,6 @@
 package com.company.errors.memory;
 
-public class Main {
+public class OutOfMemoryError {
 
     public static void main(String args[]) {
         Integer[] array = new Integer[100000 * 100000];
